55b0a7c2b8dca71f9811f1f22a23f7409cf6f67d
yangxuwei <1546079656@qq.com>
2026-03-17 15:18:39 +0800

    update ip

--- wechatReal08/deploy.sh
+++ wechatReal08/deploy.sh
@@ -20,7 +20,6 @@ docker run -d \
   --name $CONTAINER_NAME \
   --network host \
   --restart always \
-  --add-host extshort.weixin.qq.com:183.3.226.54 \
   -v "$APP_CONF_PATH:/usr/wic-go/conf/app.conf" \
   $IMAGE_NAME
 
